6e33a8fa1d87732d322763959c674c73cb18f3d4
Sarah Roesler <sarah_roesler@yahoo.com>
2022-12-13 11:20:34 -0600

    added new ai project

--- js/app.js
+++ js/app.js
@@ -40,3 +40,40 @@ const day = currentDate.getDate();
 const fullDate = monthName + '  ' + day;
 
 date.innerHTML = fullDate;
+
+
+// messing around with openAI to create animated elements. Keeping here just for fun.
+
+// var ball = document.createElement('div');
+// ball.style.width = '50px';
+// ball.style.height = '50px';
+// ball.style.borderRadius = '25px';
+// ball.style.backgroundColor = '#eb8565';
+// ball.style.position = 'absolute';
+// ball.style.zIndex = '1';
+// ball.style.top = '0px';
+// ball.style.left = '0px';
+// document.body.appendChild(ball);
+// var x = 0;
+// var y = 0;
+// var xDirection = 1;
+// var yDirection = 1;
+// var xSpeed = 1;
+// var ySpeed = 1;
+// function moveBall() {
+//     x += xDirection * xSpeed;
+//     y += yDirection * ySpeed;
+//     ball.style.left = x + 'px';
+//     ball.style.top = y + 'px';
+//     if (x + 50 >= window.innerWidth) {
+//         xDirection = -1;
+//     } else if (x <= 0) {
+//         xDirection = 1;
+//     }
+//     if (y + 50 >= window.innerHeight) {
+//         yDirection = -1;
+//     } else if (y <= 0) {
+//         yDirection = 1;
+//     }
+// }
+// setInterval(moveBall, 10);
